09b64ed61e5de79735730806b80fc55df123a754
dolores <dperezlaborda@gmail.com>
2020-07-20 03:44:24 -0300

    se agregan ejercicos basicos de JS

--- javascript/primero.js
+++ javascript/primero.js
@@ -1212,3 +1212,59 @@ const showTriangle = () =>{
 
 showTriangle();
 
+//Mostrar en consola los números de 10 en 10 desde el 0 hasta el 10000 usando for
+
+const numbers = () =>{
+    for(let numero = 10; numero < 100; numero++){ //defino la variable; la condición (expression); accionar sobre ese valor por ej. que sume o reste 
+        console.log(numero)
+    }   
+    
+}
+
+//De esta forma imprime uno x uno si quiero que imprima de 10 en 10 cambio el accionar dentro del for que seria: numero+=10; 
+
+const numbers = () =>{
+    for(let numero = 10; numero <= 100; numero+=10){ //defino la variable; la condición (expression); accionar sobre ese valor por ej. que sume o reste 
+        console.log(numero)
+    }   
+    
+}
+
+//imprime: 10, 20, 30, 40, 50, 60, 70, 80, 90
+
+//Mostrar en consola los números pares del 0 al 100 utilizando la estructura while, for y do while
+
+const showNmb = () =>{
+    number; 
+    while(number < 0 || number ){
+        console.log(number)
+    }
+}
+
+showNmb();
+
+/*
+ 1. Hacer un array que se llame "nombres" (un array vacio)
+ 2. pedirle al usuario que ingrese 5 nombres
+ 3. imprimirlos en el orden ingresado
+ 4. imprimirlos en el orden inverso
+*/
+
+const names = [];
+
+const showNames = () =>{
+    for(let i = 1; i<=5; i++){
+        pregunta = prompt("Ingresa 5 personajes de disney Pixar")
+        names.push(pregunta)
+    }
+}
+
+showNames();
+
+names;
+names.reverse();
+
+
+
+
+
